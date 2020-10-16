e997044a83792f04781393434b6e836eea04ae6b
yangjing <503>
2020-10-16 16:08:43 +0800

    提交信息

--- app/src/main/java/com/rl/a_juang/fragment/Main2Activity.java
+++ app/src/main/java/com/rl/a_juang/fragment/Main2Activity.java
@@ -1,18 +1,29 @@
 package com.rl.a_juang.fragment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Guideline;
 
 import com.rl.a_juang.R;
 
-import java.util.List;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Main2Activity extends AppCompatActivity {
+
+    @BindView(R.id.imageView2)
+    ImageView imageView2;
+    @BindView(R.id.imageView3)
+    ImageView imageView3;
+    @BindView(R.id.guideline3)
+    Guideline guideline3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gerenzhong);
+        ButterKnife.bind(this);
     }
 }
